404f9a9f2d5f34187c0c373bea556899d195f839
njfa <88588058+njfa@users.noreply.github.com>
2022-04-24 20:40:13 +0900

    Add docker.sh
    1. dockerのセットアップ用シェルを追加

---
+++ etc/os/ubuntu/init/docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+docker version || if [ -z "$(command -v docker)" ]; then
+    sudo apt-get update -y
+    sudo apt-get install -y \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+
+    sudo service docker start
+    docker version
+fi
+
+
+docker compose version || if [ -n "$(command -v docker)" ]; then
+    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+    mkdir -p $DOCKER_CONFIG/cli-plugins
+    curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+
+    chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+    docker compose version
+fi
