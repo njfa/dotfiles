46be60f0321df413bf1134ff0c23cb46667f25f2
njfa <88588058+njfa@users.noreply.github.com>
2022-09-06 02:06:53 +0900

    Add lazygit.sh

---
+++ etc/os/ubuntu/init/lazygit.sh
@@ -0,0 +1,6 @@
+if [ -z "$(command -v lazygit)" ]; then
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+    rm lazygit.tar.gz
+fi
