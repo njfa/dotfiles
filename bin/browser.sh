84b635f3a893df853d8ae88c530b0e0cb5180961
njfa <88588058+njfa@users.noreply.github.com>
2023-01-01 23:01:37 +0900

    Update browser.sh

--- bin/browser.sh
+++ bin/browser.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 # firefox
 # BROWSER="/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
@@ -7,10 +7,15 @@
 # BROWSER="/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
 
 # edge
+regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
 BROWSER="/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"
 
 if [ -n "$1" ]; then
-    TARGET=$(wslpath -w $(readlink -f $1))
+    if [[ $1 =~ $regex ]]; then
+        TARGET=$1
+    else
+        TARGET=$(wslpath -w $(readlink -f $1))
+    fi
 else
     TARGET="$HOME"
 fi
