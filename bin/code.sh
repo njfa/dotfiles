e25f4ea18798df2a270d02c936e9bd78afb95fdd
njfa <88588058+njfa@users.noreply.github.com>
2023-03-12 20:10:48 +0900

    Update code.sh

--- bin/code.sh
+++ bin/code.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-$(wslpath -u "$USERPROFILE\scoop\apps\vscode\current\bin")/code "$@"
+# $(wslpath -u "$USERPROFILE\scoop\apps\vscode\current\bin")/code "$@"
+CODEPATH=$(which.exe code | cut -c 3-)
+"$(wslpath -u "$CODEPATH")" "$@"
