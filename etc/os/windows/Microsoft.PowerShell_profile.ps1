1a66910c010ddd3e03115c003ca141d50b65c6f9
njfa <88588058+njfa@users.noreply.github.com>
2023-07-30 23:05:39 +0900

    Update Microsoft.PowerShell_profile.ps1

--- etc/os/windows/Microsoft.PowerShell_profile.ps1
+++ etc/os/windows/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@
 # if ($env:WT_PROFILE_ID) {
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme bubbles
+# Set-PoshPrompt -Theme bubbles
 # }
 
 # キーバインドをEmacs風に変更
@@ -10,10 +10,10 @@ Set-PSReadlineOption -EditMode Emacs
 
 # インストールしたコマンドのエイリアスを設定
 if ($MAJOR_VERSION -gt 5) {
-    Set-Alias ls lsd
+    # Set-Alias ls lsd
     Set-Alias cat bat
 } else {
-    Set-Alias ls lsd -O AllScope
+    # Set-Alias ls lsd -O AllScope
     Set-Alias cat bat -O AllScope
 }
 
