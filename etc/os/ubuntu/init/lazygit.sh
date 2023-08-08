bee066d88b8a080d1e194ef709045e9b034e2a4f
njfa <afjn@outlook.jp>
2023-08-08 09:49:59 +0900

    Update lazygit.sh

--- etc/os/ubuntu/init/lazygit.sh
+++ etc/os/ubuntu/init/lazygit.sh
@@ -1,6 +1,7 @@
 if [ -z "$(command -v lazygit)" ]; then
-    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-    sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
-    rm lazygit.tar.gz
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm -rf lazygit lazygit.tar.gz
 fi
