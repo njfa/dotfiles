7492c4dd9aa6ba0e3118d64728524920c74ca098
njfa <afjn@outlook.jp>
2023-08-13 22:39:17 +0900

    Update .env, zsh.sh
    1. zshのバージョンを5.9に変更
    2. zshのインストールスクリプトの処理内容を見直し

--- etc/os/ubuntu/init/zsh.sh
+++ etc/os/ubuntu/init/zsh.sh
@@ -1,25 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 
-eval "ZSH_VERSION=$ZSH_VERSION"
+is_installed=false
+
+if [ -n "$(command -v zsh)" ]; then
+    version="$(zsh --version | awk '{print $2}')"
+    echo "zsh is installed. required version: $ZSH_VERSION. now version: $version"
+
+    [ "$version" = "$ZSH_VERSION" ] && is_installed=true
+fi
 
 # zshのインストール
-if [ -z "$(command -v zsh)" -o "$(zsh --version | awk '{print $2}')" != "$ZSH_VERSION" ]; then
+if ! $is_installed; then
     sudo apt install -y wget tar make
     wget https://sourceforge.net/projects/zsh/files/zsh/$ZSH_VERSION/zsh-$ZSH_VERSION.tar.xz/download -O zsh-$ZSH_VERSION.tar.xz
     tar xvf zsh-$ZSH_VERSION.tar.xz -C ~/
     rm zsh-$ZSH_VERSION.tar.xz
-    mv ~/zsh-$ZSH_VERSION ~/.zsh
-    cd ~/.zsh
+    mv ~/zsh-$ZSH_VERSION ~/.zsh-install
+    cd ~/.zsh-install
     ./configure --enable-multibyte
-    make && sudo make install
+    make && sudo make install && rm -rf ~/.zsh-install
 fi
 
 # /etc/shellsに含まれていない場合は追加
 if [ -z "$(cat /etc/shells | grep $(which zsh))" ]; then
+    echo "zsh is not listed in /etc/shells."
+
     sudo sh -c "echo $(which zsh) >> /etc/shells"
+else
+    echo "zsh is listed in /etc/shells."
 fi
 
 # zinitのインストール
 if [ ! -d "$HOME/.zinit" ]; then
+    echo "zinit is not installed in $HOME/.zinit."
+
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+else
+    echo "zinit is installed in $HOME/.zinit."
 fi
