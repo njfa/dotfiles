39b3cd96c72917321577a2ff49816a5ddf45d20b
njfa <88588058+njfa@users.noreply.github.com>
2023-08-14 12:08:48 +0900

    Update html.lua, treesitter.lua
    1. nvim-ts-autotagはhtml以外でも使う可能性があるのでtreesitter.lua側に追記

--- .config/nvim/lua/plugins/languages/html.lua
+++ .config/nvim/lua/plugins/languages/html.lua
@@ -1,13 +1,14 @@
 local M = {}
 
 function M.load(use)
-    -- HTML入力時の補助
-    use {
-        "windwp/nvim-ts-autotag",
-        config = function()
-            require("nvim-ts-autotag").setup()
-        end
-    }
+    -- -- HTML入力時の補助
+    -- use {
+    --     "windwp/nvim-ts-autotag",
+    --     requires = 'nvim-treesitter/nvim-treesitter',
+    --     config = function()
+    --         require("nvim-ts-autotag").setup()
+    --     end
+    -- }
 end
 
 return M;
