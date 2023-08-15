3037a6604cc938f80bc43061258116cb1f6b41c4
njfa <afjn@outlook.jp>
2023-08-16 00:02:43 +0900

    Update project.lua
    1. プロジェクトと判定させる条件を変更
    2. プロジェクト選択時にディレクトリ変更する際のスコープをタブに変更

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -4,7 +4,19 @@ function M.load(use)
     use {
         "ahmedkhalf/project.nvim",
         config = function()
-            require("project_nvim").setup {}
+            require("project_nvim").setup {
+                -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+                -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+                -- order matters: if one is not detected, the other is used as fallback. You
+                -- can also delete or rearangne the detection methods.
+                detection_methods = { "pattern", "lsp" },
+
+                -- All the patterns used to detect root dir, when **"pattern"** is in
+                -- detection_methods
+                patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".env" },
+
+                scope_chdir = 'tab',
+            }
             require('telescope').load_extension('projects')
         end,
     }
