e6f500865a664044ed899bcc86de41e41ddb7031
njfa <afjn@outlook.jp>
2023-08-15 23:52:29 +0900

    Update keymaps.lua, plugins.lua, utils.lua
    1. find_files, live_grep実行時の検索対象ディレクトリをプロジェクトのルートに固定するための処理を追加

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -46,16 +46,17 @@ map("x", "<C-x>", "<C-x>gv")
 if vim.fn.exists("g:vscode") == 0 then
     -- Telescope
     -- 隠しファイルも検索対象に含めるためにrgを利用する
-    map("n", "<leader>f", "<Cmd>Telescope find_files find_command=rg,--no-ignore,--hidden,--files<CR>")
-    map("n", "<leader>F", ":Telescope find_files find_command=rg,--no-ignore,--hidden,--files search_file=")
+    map("n", "<leader>f", "<Cmd>lua find_files_from_project_git_root( { find_command={\"rg,--no-ignore,--hidden,--files\"} )<CR>")
+    map("n", "<leader>F", ":lua find_files_from_project_git_root( { find_command = {\"rg\", \"--no-ignore\", \"--hidden\", \"--files\"}, search_file=\"\"})<left><left><left>")
+    map("n", "<leader>g", "<Cmd>lua live_grep_from_project_git_root( { find_command={\"rg,--no-ignore,--hidden,--files\"} )<CR>")
+    map("n", "<leader>G", ":lua live_grep_from_project_git_root( { find_command = {\"rg\", \"--no-ignore\", \"--hidden\", \"--files\"}, glob_pattern=\"\"})<left><left><left>")
+
     map("n", "<A-s>", "<cmd>Fern . -reveal=% -drawer -toggle<cr>")
     map("n", "<leader>r", "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>")
     map("n", "<leader>b", "<Cmd>Telescope buffers<CR>")
     -- map("n", "<leader>p", "<Cmd>Telescope registers<CR>")
     map("n", "<leader>p", "<Cmd>Trouble<CR>")
     map("n", "<leader>td", "<Cmd>TodoTelescope<CR>")
-    map("n", "<leader>g", "<Cmd>Telescope live_grep find_command=rg,--no-ignore,--hidden,--files<CR>")
-    map("n", "<leader>G", ":Telescope live_grep find_command=rg,--no-ignore,--hidden,--files glob_pattern=")
     map("n", "<leader>:", "<Cmd>Telescope command_history<CR>")
     map("n", "<leader>/", "<Cmd>Telescope current_buffer_fuzzy_find<CR>")
     map("n", "<leader>e", "<Cmd>Telescope projects<CR>")
--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -39,3 +39,45 @@ function lcd_current_workspace()
         end
     end
 end
+
+function find_files_from_project_git_root(opts)
+    local function is_git_repo()
+        vim.fn.system("git rev-parse --is-inside-work-tree")
+
+        return vim.v.shell_error == 0
+    end
+
+    local function get_git_root()
+        local dot_git_path = vim.fn.finddir(".git", ".;")
+
+        return vim.fn.fnamemodify(dot_git_path, ":h")
+    end
+
+    if is_git_repo() then
+        opts = vim.tbl_extend("force", opts, {
+            cwd = get_git_root(),
+        })
+    end
+    require("telescope.builtin").find_files(opts)
+end
+
+function live_grep_from_project_git_root(opts)
+    local function is_git_repo()
+        vim.fn.system("git rev-parse --is-inside-work-tree")
+
+        return vim.v.shell_error == 0
+    end
+
+    local function get_git_root()
+        local dot_git_path = vim.fn.finddir(".git", ".;")
+        return vim.fn.fnamemodify(dot_git_path, ":h")
+    end
+
+    if is_git_repo() then
+        opts = vim.tbl_extend("force", opts, {
+            cwd = get_git_root()
+        })
+    end
+
+    require("telescope.builtin").live_grep(opts)
+end
