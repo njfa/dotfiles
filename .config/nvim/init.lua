651f61d5fdaea3e0312e194f03c93021f7d7d1d9
njfa <afjn@outlook.jp>
2023-08-23 01:06:14 +0900

    Update init.lua
    1. diagnosticsで表示する情報を文字からアイコンに変更

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,6 +62,13 @@ vim.g.cursorhold_updatetime = 100
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
+-- sign columnに表示する情報を文字ではなくアイコンに変更
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 -- Util関数の読み込み
 require('utils')
 -- プラグインの読み込み
