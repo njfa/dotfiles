01fcb8d8ffdcbd3f092b65c003ca08d884387e5e
njfa <88588058+njfa@users.noreply.github.com>
2023-08-25 02:05:02 +0900

    Update plugins.lua, lsp.lua, complete.lua
    1. LSPの設定を見直し
    2. 補完の設定を見直し

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -406,95 +406,6 @@ return require('packer').startup(function(use)
             end
         }
 
-        -- use {
-        --     'mfussenegger/nvim-jdtls',
-        --     requires = {
-        --         'williamboman/mason.nvim',
-        --     },
-        --     ft = {
-        --         "java"
-        --     },
-        --     config = function ()
-        --         local jdtls_path = vim.fn.stdpath('data') .. "/mason/packages/jdtls/bin/jdtls"
-        --         local java_debugger_path = vim.fn.stdpath('data') .. "/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"
-
-        --         local cfg = {
-        --             cmd = { jdtls_path },
-        --             root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-        --             init_options = {
-        --                 bundles = {
-        --                     vim.fn.glob(java_debugger_path, 1)
-        --                 };
-        --             },
-        --             on_attach = function(client, bufnr)
-        --                 require('jdtls').setup_dap({ hotcodereplace = 'auto' })
-        --             end
-        --         }
-
-        --         require('jdtls').start_or_attach(cfg)
-
-        --         require('dap').configurations.java = {
-        --             {
-        --                 type = 'java';
-        --                 request = 'launch';
-        --                 name = "Debug (Attach) - Remote";
-        --                 hostName = '127.0.0.1';
-        --                 port = 5005;
-        --             },
-        --         }
-        --     end
-        -- }
-
-        use {
-            'simrat39/rust-tools.nvim',
-            requires = {
-                'neovim/nvim-lspconfig',
-                'williamboman/mason.nvim',
-            },
-            ft = {
-                "rust"
-            },
-            config = function ()
-                -- local codelldb_path = require("mason-registry").get_package("codelldb"):get_install_path() .. "/extension"
-                local codelldb_path = vim.fn.stdpath('data') .. "/mason/packages/codelldb/extension"
-                local codelldb_bin = codelldb_path .. "/adapter/codelldb"
-                local liblldb_bin = codelldb_path .. "/lldb/lib/liblldb.so"
-
-                local rt = require('rust-tools')
-
-                local cfg = {
-                    server = {
-                        settings = {
-                            ['rust-analyzer'] = {
-                                cargo = {
-                                    autoReload = true
-                                }
-                            }
-                        },
-                    },
-                    dap = {
-                        adapter = require('rust-tools.dap').get_codelldb_adapter(
-                            codelldb_bin,
-                            liblldb_bin
-                        )
-                    }
-                }
-
-                rt.setup(cfg)
-
-                -- require('dap.ext.vscode').load_launchjs(nil, {rt_lldb={'rust'}})
-                require('dap').configurations.rust = {
-                    {
-                        type = 'rt_lldb';
-                        request = 'launch';
-                        name = "Debug (Attach)";
-                        cwd = "${workspaceFolder}",
-                        program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}",
-                        stopAtEntry = true,
-                    },
-                }
-            end
-        }
         -- Debuggerの設定
         local dap = require('plugins/dap')
         dap.load(use)
@@ -537,12 +448,6 @@ return require('packer').startup(function(use)
     html.load(use)
 
     use 'hashivim/vim-terraform'
-
-    -- local java = require('plugins/languages/java')
-    -- java.load(use)
-
-    -- local rust = require('plugins/languages/rust')
-    -- rust.load(use)
 
     if packer_bootstrap then
         require('packer').sync()
