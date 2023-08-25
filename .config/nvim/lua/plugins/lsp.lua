7f1dfa86ffeb2773f7658f6cb7cf000706a8c3fa
njfa <afjn@outlook.jp>
2023-08-26 03:44:30 +0900

    Update lsp.lua
    1. lspに関する見た目を更新

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,13 @@ function M.load(use)
     -- 色定義の追加
     use 'folke/lsp-colors.nvim'
 
+    use {
+        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        config = function()
+            require("lsp_lines").setup()
+        end,
+    }
+
     -- LSPサーバー管理
     use {
         'williamboman/mason.nvim',
@@ -147,6 +154,7 @@ function M.load(use)
                 lightbulb = {
                     enable = true,
                     sign = false,
+                    virtual_text = true,
                     enable_in_insert = false,
                 },
                 outline = {
@@ -171,7 +179,7 @@ function M.load(use)
         "ray-x/lsp_signature.nvim",
         config = function()
             local cfg = {
-                hint_prefix = "",
+                hint_prefix = " ",
                 floating_window_off_x = 5, -- adjust float windows x position.
                 floating_window_off_y = function() -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
                     local linenr = vim.api.nvim_win_get_cursor(0)[1] -- buf line number
