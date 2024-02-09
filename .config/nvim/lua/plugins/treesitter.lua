e192731d5e9108d1555854b9d6d4ad4d86641b20
njfa <afjn@outlook.jp>
2024-02-10 04:18:28 +0900

    Update .config/nvim
    1. rest clientを追加
    2. キーマップを見直し

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -11,7 +11,7 @@ function M.load(use)
         config = function()
             require('nvim-treesitter.configs').setup {
                 -- A list of parser names, or "all"
-                ensure_installed = { "lua", "bash", "java", "rust", "markdown", 'markdown_inline' },
+                ensure_installed = { "lua", "bash", "java", "rust", "markdown", "markdown_inline", "http", "json" },
 
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
