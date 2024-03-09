452a4f5e50d58293d7f2f85ae4e2bd5e0ed32778
njfa <afjn@outlook.jp>
2024-03-10 00:38:11 +0900

    Update nvim config
    1. trouble.nvimを再度追加
    2. lsp.luaのコメントアウト部分を削除

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -138,94 +138,6 @@ function M.load(use)
                 })
             end
         },
-        -- {
-        --     -- JavaのLSPについては専用のものを利用する
-        --     'mfussenegger/nvim-jdtls',
-        --     requires = {
-        --         'williamboman/mason.nvim',
-        --     },
-        --     ft = { "java" },
-        --     config = function ()
-        --         local jdtls_path = vim.fn.stdpath('data') .. "/mason/packages/jdtls/bin/jdtls"
-        --         local java_debugger_path = vim.fn.stdpath('data') .. "/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"
-
-        --         local cfg = {
-        --             cmd = { jdtls_path },
-        --             root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-        --             init_options = {
-        --                 bundles = {
-        --                     vim.fn.glob(java_debugger_path, true)
-        --                 };
-        --             },
-        --             on_attach = function(client, bufnr)
-        --                 -- require('jdtls').setup_dap({ hotcodereplace = 'auto' })
-        --                 on_attach_lsp(client, bufnr)
-        --             end
-        --         }
-
-        --         require('jdtls').start_or_attach(cfg)
-
-        --         -- require('dap').configurations.java = {
-        --         --     {
-        --         --         type = 'java';
-        --         --         request = 'launch';
-        --         --         name = "Debug (Attach) - Remote";
-        --         --         hostName = '127.0.0.1';
-        --         --         port = 5005;
-        --         --     },
-        --         -- }
-        --     end
-        -- },
-        -- {
-        --     'simrat39/rust-tools.nvim',
-        --     requires = {
-        --         'neovim/nvim-lspconfig',
-        --         'williamboman/mason.nvim',
-        --     },
-        --     ft = {
-        --         "rust"
-        --     },
-        --     config = function ()
-        --         -- local codelldb_path = require("mason-registry").get_package("codelldb"):get_install_path() .. "/extension"
-        --         local codelldb_path = vim.fn.stdpath('data') .. "/mason/packages/codelldb/extension"
-        --         local codelldb_bin = codelldb_path .. "/adapter/codelldb"
-        --         local liblldb_bin = codelldb_path .. "/lldb/lib/liblldb.so"
-
-        --         local rt = require('rust-tools')
-
-        --         local cfg = {
-        --             server = {
-        --                 settings = {
-        --                     ['rust-analyzer'] = {
-        --                         cargo = {
-        --                             autoReload = true
-        --                         }
-        --                     }
-        --                 },
-        --             },
-        --             dap = {
-        --                 adapter = require('rust-tools.dap').get_codelldb_adapter(
-        --                     codelldb_bin,
-        --                     liblldb_bin
-        --                 )
-        --             }
-        --         }
-
-        --         rt.setup(cfg)
-
-        --         -- require('dap.ext.vscode').load_launchjs(nil, {rt_lldb={'rust'}})
-        --         require('dap').configurations.rust = {
-        --             {
-        --                 type = 'rt_lldb';
-        --                 request = 'launch';
-        --                 name = "Debug (Attach)";
-        --                 cwd = "${workspaceFolder}",
-        --                 program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}",
-        --                 stopAtEntry = true,
-        --             },
-        --         }
-        --     end
-        -- }
     }
 
     use {
@@ -304,6 +216,10 @@ function M.load(use)
             }
             require("lsp_signature").setup(cfg)
         end
+    }
+
+    use {
+        "folke/trouble.nvim"
     }
 end
 
