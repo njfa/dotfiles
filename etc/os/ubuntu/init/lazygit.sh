9dd2e5bbbbc96b363172464c56e00f20549057a1
njfa <afjn@outlook.jp>
2024-04-20 22:10:34 +0900

    Update etc/os/ubuntu, ubuntu-22.04
    ubuntu/init
    1. dockerのインストールコマンドを修正
    2. rustのインストールコマンドを修正
    3. lazygitのインストールコマンドを修正
    4. 不要なツールを削除
    
    ubuntu-22.04/init
    1.deltaインストール済みの場合の挙動を改善
    2.deltaのバージョンを最新のものに更新

--- etc/os/ubuntu/init/lazygit.sh
+++ etc/os/ubuntu/init/lazygit.sh
@@ -3,17 +3,16 @@
 is_installed=false
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+LAZYGIT_INSTALLED_VERSION=""
 
-if [ -n "$(command -v lazygit)" ]; then
-    version=$(lazygit --version | awk '{print $6}' | grep -o 'version=[^,]*' | awk -F= '{print $2}')
-    echo "lazygit is installed. required version: $LAZYGIT_VERSION. now version: $version"
-
-    [ "$version" = "$LAZYGIT_VERSION" ] && is_installed=true
+if command -v lazygit >/dev/null 2>&1; then
+    LAZYGIT_INSTALLED_VERSION=$(lazygit --version | awk '{print $6}' | grep -o 'version=[^,]*' | awk -F= '{print $2}')
+    echo "lazygit is installed. required version: $LAZYGIT_VERSION. now version: $LAZYGIT_INSTALLED_VERSION"
 else
     echo "lazygit is not installed. required version: $LAZYGIT_VERSION."
 fi
 
-if ! $is_installed; then
+if [ -n "$LAZYGIT_VERSION" -a "$LAZYGIT_VERSION" != "$LAZYGIT_INSTALLED_VERSION" ]; then
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit /usr/local/bin
--- etc/os/ubuntu/init/rust.sh
+++ etc/os/ubuntu/init/rust.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ -z "$(command -v cargo)" ]; then
-    curl https://sh.rustup.rs -sSf | xargs -0 -I {} sh -c {} rustup -y
+if ! cargo version 2>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-if [ ! -z "$(command -v fish)" -a -z "$(fish -c 'echo $fish_user_paths' | grep .cargo)" ]; then
-    fish -c 'set -Ux fish_user_paths $HOME/.cargo/bin $fish_user_paths'
-fi
