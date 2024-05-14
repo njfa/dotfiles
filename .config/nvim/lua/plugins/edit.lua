b721cb971a7ae18425eac0a65f11d32ff3db9dfb
njfa <afjn@outlook.jp>
2024-05-14 09:46:20 +0900

    Update dial.nvim config

--- .config/nvim/lua/plugins/edit.lua
+++ .config/nvim/lua/plugins/edit.lua
@@ -29,6 +29,17 @@ return {
             map("v", "<C-x>", require("dial.map").dec_visual(),   {silent = true})
             map("v", "g<C-a>", require("dial.map").inc_gvisual(), {silent = true})
             map("v", "g<C-x>", require("dial.map").dec_gvisual(), {silent = true})
+
+            local augend = require("dial.augend")
+            require("dial.config").augends:register_group{
+                -- default augends used when no group name is specified
+                default = {
+                    augend.integer.alias.decimal,   -- nonnegative decimal number (0, 1, 2, 3, ...)
+                    augend.integer.alias.hex,       -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+                    augend.date.alias["%Y/%m/%d"],  -- date (2022/02/19, etc.)
+                    augend.constant.alias.bool,    -- boolean value (true <-> false)
+                }
+            }
         end
     },
 
