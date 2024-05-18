aba96622bc275cd39ecef42e1219ed1e61528d11
njfa <afjn@outlook.jp>
2024-05-18 23:16:05 +0900

    Update nvim config

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,25 +1,23 @@
 return {
-    "mfussenegger/nvim-dap",
-    "nvim-neotest/nvim-nio",
-
     {
         "rcarriga/nvim-dap-ui",
-        dependencies = {
-            "mfussenegger/nvim-dap"
-        },
+        dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
         config = function()
             require("dapui").setup()
 
             local dap, dapui = require("dap"), require("dapui")
-            dap.listeners.after.event_initialized["dapui_config"] = function()
+            dap.listeners.before.attach.dapui_config = function()
                 dapui.open()
             end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
                 dapui.close()
             end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
+            -- dap.listeners.before.event_exited.dapui_config = function()
+            --     dapui.close()
+            -- end
         end
     },
 
