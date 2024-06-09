ab12ffa13beacfcf2214f5c4400b208ae7d8b624
njfa <afjn@outlook.jp>
2024-06-09 23:40:56 +0900

    Update nvim config
    プラグインの設定を見直し

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,15 +1,4 @@
 return {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
-    'ray-x/cmp-treesitter',
-    'petertriho/cmp-git',
-    'onsails/lspkind.nvim',
-
     -- 自動補完
     {
         'hrsh7th/nvim-cmp',
