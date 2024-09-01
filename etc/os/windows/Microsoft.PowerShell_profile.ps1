03aa282ec18744a6c5776d0510963c7f13f1b9e0
njfa <afjn@outlook.jp>
2024-09-02 00:48:52 +0900

    Update Microsoft.PowerShell_profile.ps1

--- etc/os/windows/Microsoft.PowerShell_profile.ps1
+++ etc/os/windows/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,7 @@ Import-Module posh-git
 
 # キーバインドをEmacs風に変更
 Set-PSReadlineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteChar
 
 # インストールしたコマンドのエイリアスを設定
 if ($PSVersionTable.PSVersion.Major -gt 5) {
@@ -15,10 +16,12 @@ if ($PSVersionTable.PSVersion.Major -gt 5) {
     # Set-Alias ls lsd
     Set-Alias cat bat
     Set-Alias vim nvim
+    Set-Alias map Get-PSReadLineKeyHandler
 } else {
     # Set-Alias ls lsd -O AllScope
     Set-Alias cat bat -O AllScope
     Set-Alias vim nvim -O AllScope
+    Set-Alias map Get-PSReadLineKeyHandler -O AllScope
 }
 
 oh-my-posh init pwsh --config "$env:USERPROFILE\.dotfiles\etc\os\windows\night-owl.omp.json" | Invoke-Expression
