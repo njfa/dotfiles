055626ee209e38ed96b5d687a146f47ef8da2e6a
njfa <afjn@outlook.jp>
2024-10-08 10:40:50 +0900

    Update conform.nvimに関連する設定を更新

--- .config/nvim/lua/plugins/edit.lua
+++ .config/nvim/lua/plugins/edit.lua
@@ -277,24 +277,27 @@ return {
     },
 
     {
-        'stevearc/conform.nvim',
+        "stevearc/conform.nvim",
         opts = {},
         config = function()
             require("conform").setup({
                 formatters_by_ft = {
-                    lua = { "stylua" },
+                    lua = {
+                        command = "stylua",
+                        args = { "--indent-type", "Spaces" },
+                    },
                     -- Conform will run multiple formatters sequentially
                     python = { "isort", "black" },
                     -- You can customize some of the format options for the filetype (:help conform.format)
                     rust = { "rustfmt", lsp_format = "fallback" },
                     -- Conform will run the first available formatter
-                    javascript = { "prettier" --[["prettierd", "prettier", stop_after_first = true ]] },
+                    javascript = {
+                        "prettier" --[["prettierd", "prettier", stop_after_first = true ]],
+                    },
                     bash = { "shfmt" },
                     java = { "google-java-format" },
                 },
             })
-
-            vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
         end,
-    }
+    },
 }
