42f8ed5ec32c4c215ea7cb42535c32c7f275f5dc
njfa <afjn@outlook.jp>
2025-02-18 02:48:40 +0900

    feat: プロジェクトのファイル検索とGrep検索の改善
    - `find_files_from_project_git_root`関数に`.git`ディレクトリを除外するオプションを追加
    - `live_grep_from_project_git_root`関数からデフォルトの`glob_pattern`設定を削除
    - `telescope.nvim`の設定で`layout_strategy`と`layout_config`を各ピッカーに追加
    - `telescope-ui-select.nvim`拡張を追加- `which-key`のGrep検索のキーマッピングを更新

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,66 +1,43 @@
 -- ファジーファインダー
 return {
-    -- telescopeでプロジェクトの一覧を表示するのに利用する
-    {
-        "ahmedkhalf/project.nvim",
-        config = function()
-            require("project_nvim").setup {
-                -- Methods of detecting the root directory. **"lsp"** uses the native neovim
-                -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
-                -- order matters: if one is not detected, the other is used as fallback. You
-                -- can also delete or rearangne the detection methods.
-                detection_methods = { "pattern", "lsp" },
-
-                -- All the patterns used to detect root dir, when **"pattern"** is in
-                -- detection_methods
-                patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".env", ".gitlab-ci.yml" },
-
-                scope_chdir = 'tab',
-
-                datapath = vim.fn.stdpath("data")
-            }
-        end
-    },
-
-    {
-        'nvim-telescope/telescope-dap.nvim',
-        dependencies = {
-            'mfussenegger/nvim-dap',
-        }
-    },
-
-    {
-        "nvim-telescope/telescope-frecency.nvim"
-    },
-
     {
         'nvim-telescope/telescope.nvim',
         branch = 'master',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-telescope/telescope-frecency.nvim',
-            'nvim-telescope/telescope-dap.nvim',
-            "ahmedkhalf/project.nvim",
+            'nvim-telescope/telescope-ui-select.nvim',
+            {
+                'nvim-telescope/telescope-dap.nvim',
+                dependencies = {
+                    'mfussenegger/nvim-dap',
+                }
+            },
+            {
+                "ahmedkhalf/project.nvim",
+                config = function()
+                    require("project_nvim").setup {
+                        -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+                        -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+                        -- order matters: if one is not detected, the other is used as fallback. You
+                        -- can also delete or rearangne the detection methods.
+                        detection_methods = { "pattern", "lsp" },
+
+                        -- All the patterns used to detect root dir, when **"pattern"** is in
+                        -- detection_methods
+                        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".env", ".gitlab-ci.yml" },
+
+                        scope_chdir = 'tab',
+
+                        datapath = vim.fn.stdpath("data")
+                    }
+                end
+            },
         },
         config = function()
             local actions = require("telescope.actions")
             require('telescope').setup {
                 defaults = {
-                    layout_strategy = "vertical",
-                    layout_config = {
-                        horizontal = {
-                            height = 0.99,
-                            preview_cutoff = 40,
-                            prompt_position = "bottom",
-                            width = 0.99
-                        },
-                        vertical = {
-                            height = 0.99,
-                            preview_cutoff = 40,
-                            prompt_position = "bottom",
-                            width = 0.99
-                        }
-                    },
                     mappings = {
                         i = {
                             ["<esc>"] = actions.close
@@ -76,25 +53,74 @@ return {
                         '--smart-case',
                         '--hidden',
                         '--no-ignore',
-                        '--trim'
+                        '--trim',
+                        "--glob",
+                        "!**/.git/*"
                     },
                     file_ignore_patterns = {
                         "node_modules/",
                         "target/"
                     }
                 },
+                pickers = {
+                    find_files = {
+                        layout_strategy = "vertical",
+                        layout_config = {
+                            height = 0.99,
+                            preview_cutoff = 40,
+                            prompt_position = "bottom",
+                            width = 0.99,
+                            preview_height = 15,
+                        },
+                    },
+                    oldfiles = {
+                        layout_strategy = "vertical",
+                        layout_config = {
+                            height = 0.99,
+                            preview_cutoff = 40,
+                            prompt_position = "bottom",
+                            width = 0.99,
+                            preview_height = 15,
+                        },
+                    },
+                    live_grep = {
+                        layout_strategy = "vertical",
+                        layout_config = {
+                            height = 0.99,
+                            preview_cutoff = 40,
+                            prompt_position = "bottom",
+                            width = 0.99
+                        },
+                    },
+                },
                 extensions = {
                     frecency = {
-                        db_root = vim.fn.stdpath("data"),
                         show_scores = true,
-                        ignore_patterns = { "*.git/*", "*/tmp/*" },
-                    }
+                        ignore_patterns = { "**/.git/*" },
+                        layout_strategy = "vertical",
+                        layout_config = {
+                            height = 0.99,
+                            preview_cutoff = 40,
+                            prompt_position = "bottom",
+                            width = 0.99,
+                            preview_height = 15,
+                        },
+                    },
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                            layout_config = {
+                                width = 100,
+                            },
+                            -- even more opts
+                        }
+                    },
                 },
             }
 
             require('telescope').load_extension('dap')
             require('telescope').load_extension('projects')
             require('telescope').load_extension("frecency")
+            require('telescope').load_extension("ui-select")
         end
     },
 }
