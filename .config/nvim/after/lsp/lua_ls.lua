3b50855c94017e39b5f8426c7b8980ae640c2691
njfa <afjn@outlook.jp>
2025-06-17 10:13:17 +0900

    feat(lsp): on_attachおよびcapabilities設定の共通化に伴うlua_lsとpylspの個別設定削除
    - lua_ls.luaおよびpylsp.luaからon_attachとcapabilitiesの設定を削除
    - 共通処理への移行により各LSP設定ファイルの記述を簡素化

--- .config/nvim/after/lsp/lua_ls.lua
+++ .config/nvim/after/lsp/lua_ls.lua
@@ -8,13 +8,5 @@ return {
                 },
             },
         },
-    },
-    on_attach = function(_, bufnr)
-        require("common").on_attach_lsp(_, bufnr)
-    end,
-    capabilities = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol
-        .make_client_capabilities())
-    -- capabilities = require("cmp_nvim_lsp").default_capabilities(
-    --     vim.lsp.protocol.make_client_capabilities()
-    -- ),
+    }
 }
